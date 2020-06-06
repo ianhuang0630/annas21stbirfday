790c4f913c481d0b2f3df71207f3611b3f2ac9f3
Ian Huang <iyh2110@columbia.edu>
2020-06-06 16:38:58 -0400

    basics

---
+++ index.js
@@ -0,0 +1,28 @@
+window.open = function() {};
+            window.print = function() {};
+            // Support hover state for mobile.
+            if (false) {
+                window.ontouchstart = function() {};
+            }
+
+function generate_confetti(){
+    document.createElement()  
+}
+
+function throw_confetti(){
+    // x = u * t * cos(\theta)
+    // y = u * t * sin(\theta) - 1/2 g t^2
+
+}
+
+function main(){
+    // generate confetti outside window
+    
+
+    // what a little 
+    
+
+    // projectile!
+}
+
+
