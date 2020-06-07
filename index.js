80cb3359c855dcb497cbab3eb233c242c9310db4
Ian Huang <iyh2110@columbia.edu>
2020-06-06 23:40:35 -0400

    changes

--- index.js
+++ index.js
@@ -6,46 +6,58 @@ window.open = function() {};
             }
 
 function generate_confetti(){
-    var newDiv = document.createElement("div")  ;
-    var cup = document.createElement("IMG");
-    cup.src = `assets/solocup_clear.png`;
-    cup.height = 100;
-    cup.width = 100;
-    newDiv.appendChild(cup);
     
-    newDiv.style.position='absolute';
-    newDiv.style.left='0px'; 
-    newDiv.style.top='0px';
-    document.getElementById("my-canvas").appendChild(newDiv);
-    
-    // start the rotations
-    newDiv.classList.add("rotate"); 
-    
+    var divs = [];
+    var dxs = [];
+    var dys= [];
+
+    for (var i=0 ; i < 25 ; i++){
+        var newDiv = document.createElement("div")  ;
+        var cup = document.createElement("IMG");
+        cup.src = `assets/solocup_clear.png`;
+        cup.height = 100;
+        cup.width = 100;
+        newDiv.appendChild(cup);
+        
+        newDiv.style.position='absolute';
+        newDiv.style.left='0px'; 
+        newDiv.style.top='0px';
+        document.getElementById("my-canvas").appendChild(newDiv);
+        
+        // start the rotations
+        var rotate_group = "rotate".concat(  Math.round(1+Math.random()).toString() );
+        newDiv.classList.add(rotate_group);
+        console.log(rotate_group);
+        divs.push(newDiv);
+        var noisedx = Math.random() * 1000;
+        var noisedy = Math.random() * 1000;
+        var dx = noisedx;
+        dxs.push(dx); 
+        var dy = 50 + noisedy;
+        dys.push(dy);
+    }
     // add to the rotate list
-    
-    var noisedx = Math.random() * 1000;
-    var noisedy = Math.random() * 1000;
-    var dx = noisedx;
-    var dy = 50 + noisedy;
-    throw_confetti(0, newDiv, dx, dy);
+
+    throw_confetti(0, divs, dxs, dys);
 }
 
-function throw_confetti(timecount, div, dx, dy){
+function throw_confetti(timecount, divs, dxs, dys){
     if (timecount<200){
         var next_timecount = timecount + 1;
-        var x = timecount/1000*20  * dx;
-        var y = (timecount/1000*20) * (timecount/1000*20) * 0.5 * dy;
-
-        div.style.left=Math.round(x).toString().concat('px');
-        div.style.top=Math.round(y).toString().concat('px');
+        for (var i=0; i<25; i++){
+            var x = timecount/1000*20  * dxs[i];
+            var y = (timecount/1000*20) * (timecount/1000*20) * 0.5 * dys[i];
+            divs[i].style.left = Math.round(x).toString().concat('px');
+            divs[i].style.top = Math.round(y).toString().concat('px');
+        }
         
-        setTimeout(throw_confetti.bind(null, next_timecount, div, dx, dy), 20);
+        setTimeout(throw_confetti.bind(null, next_timecount, divs, dxs, dys), 20);
     }
     else{
-        div.classList.add("hide");
+        for (var i=0; i<25; i++){
+            divs[i].classList.add("hide");
+        } 
     }
-    // x = u * t * cos(\theta)
-    // y = u * t * sin(\theta) - 1/2 g t^2
 }
 
 function setCandleListener(){
@@ -58,21 +70,13 @@ function getPartyStarted(){
     var player = document.getElementById("GoodTimeGirl");
     player.play();
     
-    for (var i=0; i<50; i++){
-        generate_confetti() ;
-    }     
+    generate_confetti() ;
 }
 
 function main(){
     // play
     setCandleListener();
     console.log("hello");
-
-    // generate confetti outside window
-
-        
-
-    // projectile!
 }
 
 main();
